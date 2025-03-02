b5fc54b3174c56e7468916daf497c75457755c47
agimuche1 <agimuche1@gmail.com>
2025-03-02 01:04:09 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.querySelector(".nav-links");
-    const hamburger = document.querySelector(".hamburger");
-
-    hamburger.addEventListener("click", () => {
-        menu.style.display = menu.style.display === "block" ? "none" : "block";
+// smooth scrolling effect 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e)){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-});
+    
+    //Fade-in animation on scroLL
+    const sections = document.querySelectorAll("section");
+
+    const observer = new intersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+            }
+        });
+    }, { threshold: 0.1 });
+    
+    section.forEach(section => {
+        section.style.opacity = 0;
+        observer.observer(section);
+    });
